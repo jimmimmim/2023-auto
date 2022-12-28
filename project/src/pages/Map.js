91e622fc93b31306645ac01145bb567073bbb834
JIMIN KIM <likecharry@gmail.com>
2022-12-28 17:01:16 +0900

    [Feat] Add react-leaflet map

--- project/src/pages/Map.js
+++ project/src/pages/Map.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 export default function Map() {
   return (
-    <h1>Map</h1>
+    <div id='map' className='h-96 w-4/5'>
+    <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={true}
+          style={{ height: "100%", width: "100%" }}
+        >
+          <TileLayer
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+      </MapContainer>
+    </div>
   );
 }
