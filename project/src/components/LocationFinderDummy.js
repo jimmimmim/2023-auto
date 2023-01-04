845af525add8b5b811fcd1266edb68054eb10af5
JIMIN KIM <likecharry@gmail.com>
2023-01-04 13:27:27 +0900

    Add Layer Control

---
+++ project/src/components/LocationFinderDummy.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { useMapEvents, Popup } from 'react-leaflet';
+
+// return location at which the click occurred
+export default function LocationFinderDummy() {
+
+    const [location, setLocation] = useState('');
+
+    const map = useMapEvents({
+        click(e) {
+            // console.log(e.latlng);
+            setLocation(e.latlng);
+            if (location.lat) {
+                console.log('lat: ', location.lat);
+                console.log('lng: ', location.lng);
+            }
+        },
+    });
+
+    return null;
+};
--- project/src/pages/Dashboard.js
+++ project/src/pages/Dashboard.js
@@ -1,12 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup, GeoJSON, Polyline } from 'react-leaflet';
+import React, { useEffect, useState, useRef } from "react";
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON, Polyline, LayersControl, useMapEvent } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import axios from 'axios';
 
+import LocationFinderDummy from "../components/LocationFinderDummy";
 
+import ddmgrids from '../data/ddmgrids.json';
 import seouluniv from '../data/seouluniv1010.json';
  
 export default function Map() { 
+
+  function MapComponent() {
+    const map = useMapEvent('zoom', () => {
+      map.setView([37.58360620664327, 127.05843925233872]);
+    })
+    const location = useMapEvent('click', () => {
+      if (location.getZoom() == 18) {
+        console.log('max zoom');
+        // location.addLayer();
+      }
+      console.log(location.getZoom());
+    })
+    return null
+  }
+
 
   const [data, setData] = useState(['']);
 
@@ -16,24 +33,28 @@ export default function Map() {
       .then(res => setData(res.data))
   }
 
-  // id popup
-  const onEachFeature = (feature, layer) => {
-    layer.bindPopup('<h1 class="font-bold">'+feature.geometry.type+'</h1><p>id: '+feature.properties.id+'</p>');
-  };
+  // grid - id popup
+  // const onEachFeature = (feature, layer, e) => {
+  //   layer.bindPopup('<h1 class="font-bold">'+feature.geometry.type+'</h1><p>id: '+feature.properties.id+'</p>');
+  // };
   
   const confirmedStyle = (feature) => {
-      var confirmed = feature.properties.val;
+      // const confirmed = feature.properties.val;
+      const confirmed = null;
       if (confirmed) {
         return {
+          weight: 1, // stroke width (default: 3)
           color: 'yellow',
           fillcolor: 'yellow', 
           fillOpacity: 0.3
         }
       } else {
         return {
-          color: 'white',
+          color: 'white', // stroke color
+          weight: 1, // stroke width (default: 3)
+          opacity: 1, // stroke opacity (default: 1.0)
           fillcolor: 'white',
-          fillOpacity: 0.1
+          fillOpacity: 0
         }
       } 
   };
@@ -63,8 +84,6 @@ export default function Map() {
       polylines[i] = temp;
     }
   }
-
-  // console.log(polylines)
 
   // Create a polygon geometry
   const polygon = {
@@ -99,28 +118,47 @@ export default function Map() {
   // }
 
   return (
-    <>
-    <div id='map' className='w-4/5' style={{height: '800px'}}>
-      <MapContainer
-        center={[37.58360620664327, 127.05843925233872]} // 서울시립대
-        zoom={20}
-        scrollWheelZoom={true} 
-        style={{ height: "100%", width: "100%" }}
-      >
-        <TileLayer
-          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        />
-        <GeoJSON 
-          data={seouluniv} 
-          onEachFeature={onEachFeature}
-          style={confirmedStyle}
-        />
-        {
-          polylines.map((polyline, i) => (
-            <Polyline key={i} pathOptions={lineOptions[i]} positions={polyline} />
-          ) )
-        }
-      </MapContainer>
+    <div className="w-full">
+      <div className="flex border-b border-l border-r border-indigo-400 border-">
+        <div id='map' className='w-4/5' style={{height: '800px'}}>
+          <MapContainer
+            center={[37.58360620664327, 127.05843925233872]} // 서울시립대
+            zoom={18} // max: 18
+            scrollWheelZoom={true}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <LocationFinderDummy />
+            <MapComponent />
+            <TileLayer
+              url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            />
+            {
+              polylines.map((polyline, i) => (
+                <Polyline key={i} pathOptions={lineOptions[i]} positions={polyline} />
+              ) )
+            }
+            <LayersControl>
+              <LayersControl.Overlay checked name="10m 격자">
+              <GeoJSON 
+                data={seouluniv} 
+                // onEachFeature={onEachFeature}
+                style={confirmedStyle}
+              />
+              </LayersControl.Overlay>
+              <LayersControl.Overlay name="100m 격자">
+              <GeoJSON 
+                data={ddmgrids} 
+                // onEachFeature={onEachFeature}
+                style={confirmedStyle}
+              />
+              </LayersControl.Overlay>
+            </LayersControl>
+          </MapContainer>
+        </div>
+        <div id='board' className="w-1/5 border-l-2 border-indigo-200">
+          <div className="p-0 text-lg text-left border-b border-indigo-400">
+          </div>
+        </div>
       </div>
       <div>
         <button onClick={getClick} className="hover:text-red-600">Click</button>
@@ -128,7 +166,7 @@ export default function Map() {
       {data.map((v,i) => 
         v.map((w, j) => {
           return (
-            <div key={j} className="flex border border-red-400">
+            <div key={j} className="flex text-sm border-b border-indigo-400">
               <div className="mx-8">id = {w.id}</div>
               <div className="mr-8">times = {w.times}</div>
               <div className="mr-8">latlon = {w.lat}, {w.lon}</div>
@@ -136,6 +174,6 @@ export default function Map() {
           )
         })
       )}
-      </>
+    </div>
   );
 }
