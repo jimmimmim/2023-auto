ad8efb449a62289b0effe38ef08666c14cc049de
JIMIN KIM <likecharry@gmail.com>
2023-02-02 16:14:40 +0900

    Update Grid Crosspoint

--- project/src/pages/Map.js
+++ project/src/pages/Map.js
@@ -21,7 +21,6 @@ import MapZoomComponent from '../components/MapZoomComponent';
 import Legend from "../components/Legend";
 
 // **** Files ***** 
-import ddmgrids from '../data/ddmgrids.json'; // 100m grid
 // import seouluniv_10m from '../data/seouluniv10104326.json'; // 10m grid
 import seouluniv_10m from '../data/polygon10m_0112.json'; // 10m grid
 // import seouluniv_polygon from '../data/seouluniv1mpolygon.json'; // 1m grid - polygon
@@ -151,11 +150,6 @@ export default function Map() {
   const labeled_polylines = {}; // polyline 각각의 정보를 담을 객체
   const polylines = [];
 
-  // console.log(data);
-
-
-
-
   // 고유아이디+날짜+시각
   const phone_date = Object.keys(labeled_polylines);
 
@@ -214,7 +208,6 @@ export default function Map() {
 //     return selected;
 //   };
 
-
   // extract latlong
   // console.log(Object.keys(selectedPolyline));
 
@@ -235,76 +228,31 @@ export default function Map() {
 
   console.log('selected_polylines: ', selected_polylines);
 
-  // get original_id by robot name
-//   for (let i = 0; i < selectedPolylines.length; i++) {
-//     for (let j = 0; j < checkbox_info.length; j++) {
-//       if (checkbox_info[j]['name'] === selectedPolylines[i]) {
-//         // console.log(checkbox_info[j]['original_id']);
-//         // console.log(labeled_polylines[checkbox_info[j]['original_id']]); // 선택된 original id에 해당되는 폴리라인 출력
-//         selected_polylines[i] = labeled_polylines[checkbox_info[j]['original_id']];
-//       }
-//     }
-//   }
-
-//   // get original_id by robot name
-//   for (let i = 0; i < selectedPolylines.length; i++) {
-//     for (let j = 0; j < robot_items.length; j++) {
-//       if (robot_items[j]['name'] === selectedPolylines[i]) {
-//         // console.log(robot_items[j]['original_id']);
-//         // console.log(labeled_polylines[robot_items[j]['original_id']]); // 선택된 original id에 해당되는 폴리라인 출력
-//         // selected_polylines[i] = labeled_polylines[robot_items[j]['id']];
-//         console.log(`robot_items[${j}]["id"]: `, robot_items[j]['id']);
-//       }
-//     }
-//   }
-
-//   console.log('robot_items: ', robot_items);
-  // console.log(selected_polylines);
-
-  // console.log(gridData);
   const setDisplayFlex = () => {
     console.log('popup closed!!!!!!!!!!!!!');
   }
 
-  let gids = [];   // robots 배열의 내부 배열 담을 변수
   let robots = []; // 차량별로 지나가는 격자 아이디 배열
-
-  console.log(selectedCars);
-  console.log(selectedPolyline);
+  let values = {}; // 격자 아이디별 통행량(지나가는 선 개수) 담을 딕셔너리
+  
+  // 지나가는 격자 아이디 배열 생성하기
+  // 3m, 5m, 10m 아이디 수집
   for (let i = 0; i < selectedCars.length; i++) {
+    let gids = [];   // robots 배열의 내부 배열 담을 변수
     if (selectedPolyline[selectedCars[i]]) {
       for (let j = 0; j < selectedPolyline[selectedCars[i]].length; j++){
-        // console.log(selectedPolyline[selectedCars[i]][j]['id_3m']);
-        // console.log(selectedPolyline[selectedCars[i]][j]['id_5m']);
         gids.push(selectedPolyline[selectedCars[i]][j]['id_3m']);
         gids.push(selectedPolyline[selectedCars[i]][j]['id_5m']);
         // gids.push(selectedPolyline[selectedCars[i]][j]['id_10m']);
       }
     }
-    gids = [...new Set(gids.sort())]; // 중복제거
+    gids = [...new Set(gids.sort())]; // 차량 내 격자아이디 중복 제거, 아이디순 정렬(편의상)
+    // console.log(gids);
     robots.push(gids);
+    // console.log(robots);
   }
 
   console.log('robots: ', robots);
-
-//   // 지나가는 격자 아이디 배열 생성하기
-//   // 3m, 5m 아이디 한번에 수집
-//   for (let i = 0; i < data.length; i++) {
-//     for (let j = 0; j < data[i].length; j++) {
-//       gids.push(data[i][j]['id_3m']);
-//       gids.push(data[i][j]['id_5m']);
-//     }
-//     // console.log(data[i].length);
-//     gids = [...new Set(gids.sort())]; // 중복제거
-//     // console.log(gids);
-//     robots.push(gids);
-//   }
-
-  // console.log('robots: ', robots);
-  
-  
-  // 격자 아이디별 지나가는 선 개수 담을 딕셔너리
-  let values = {};
 
   // (1) 아이디별 value 저장 - total
   for (let i = 0; i < robots.length; i++) {
@@ -314,35 +262,7 @@ export default function Map() {
     }
   }
 
-  // for (let i = 0; i < gids.length; i++) {
-  //   if (!values[gids[i]]) values[gids[i]] = 0;
-  //   values[gids[i]]++;
-  // }
-
   console.log('values: ', values); // gid별 통행량
-
-  // 차량 배열을 전달받아 선택 차량에 대한 격자(통행량) 시각화
-  // (2) 특정 차량에 대해서만 아이디별 value 저장
-  // 예) 0번, 2번 차량 선택
-  // 체크박스로 차량 선택 후 해당 인덱스를 checkedCars 배열로 전달한다고 가정
-
-  // values = {}; // 함수 안에 들어가면 const로 바꾸기
-
-  // const checkedCars = [0, 1, 2];
-
-  // for (let i = 0; i < checkedCars.length; i++) {
-
-  //     for (let j = 0; j < robots[checkedCars[i]][j]; j++) {
-  //         if (!values[robots[checkedCars[i]][j]]) {
-  //           values[robots[checkedCars[i]][j]] = 0;
-  //         }
-  //         values[robots[checkedCars[i]][j]]++;
-  //     }
-  // }
-
-// // value 모두 계산되었다고 가정하고 화면에 표시
-// const testValues = {185748.0: 3, 185749.0: 1, 185774.0: 3, 185775.0: 3, 185773.0: 1, 185766.0: 3, 185767.0: 3, 185764.0: 1, 185765.0: 1, 186302.0: 2, 186301.0: 1, 186292.0: 3, 186293.0: 2, 186290.0: 3, 186291.0: 3, 186288.0: 3, 186289.0: 3, 186286.0: 3, 186287.0: 3, 186285.0: 3, 186276.0: 3, 186275.0: 2, 184720.0: 2, 184721.0: 2, 184718.0: 3, 184719.0: 1, 184716.0: 3, 184717.0: 3, 184715.0: 1, 184694.0: 3, 185246.0: 3, 185247.0: 2, 185245.0: 2, 185242.0: 3, 185243.0: 1, 185240.0: 2, 185241.0: 3, 185221.0: 3, 185248.0: 2, 187866.0: 3, 187867.0: 2, 187857.0: 3, 188392.0: 3, 188393.0: 3, 188390.0: 3, 188391.0: 3, 188388.0: 3, 188389.0: 3, 188387.0: 2, 188384.0: 3, 188385.0: 1, 186812.0: 3, 186813.0: 1, 186803.0: 3, 187340.0: 1, 187339.0: 2, 187330.0: 3, 181532.0: 2, 181533.0: 2, 181558.0: 3, 181559.0: 1, 182060.0: 1, 182059.0: 3, 182085.0: 3, 180478.0: 1, 180479.0: 2, 180504.0: 2, 180505.0: 3, 181006.0: 2, 181005.0: 1, 181032.0: 1, 181031.0: 2, 183640.0: 3, 183666.0: 3, 183667.0: 1, 184194.0: 2, 184193.0: 3, 184167.0: 3, 182586.0: 3, 182612.0: 3, 183113.0: 3, 183139.0: 3, 189440.0: 2, 189966.0: 3, 189967.0: 2, 189965.0: 1, 188918.0: 1, 188919.0: 1, 188916.0: 1, 188917.0: 2, 188914.0: 3, 188915.0: 1, 188912.0: 3, 188913.0: 3, 188911.0: 1, 189439.0: 3, 190492.0: 1, 169439.0: 2, 169428.0: 3, 169427.0: 3, 169440.0: 2, 169966.0: 2, 169967.0: 1, 169954.0: 3, 169953.0: 3, 168383.0: 1, 168374.0: 3, 168375.0: 3, 168384.0: 3, 168385.0: 2, 168912.0: 3, 168911.0: 1, 168900.0: 1, 168901.0: 3, 171020.0: 2, 171021.0: 1, 171548.0: 1, 171547.0: 2, 171532.0: 3, 171533.0: 2, 170494.0: 2, 170493.0: 2, 170480.0: 3, 170479.0: 2, 171006.0: 3, 167326.0: 3, 167327.0: 2, 167324.0: 3, 167325.0: 3, 167322.0: 3, 167323.0: 3, 167384.0: 3, 167856.0: 3, 167857.0: 2, 167854.0: 3, 167855.0: 3, 167852.0: 2, 167853.0: 1, 167851.0: 1, 167848.0: 3, 167849.0: 3, 177318.0: 1, 177317.0: 2, 177345.0: 3, 177844.0: 3, 177845.0: 1, 177843.0: 1, 177872.0: 3, 177873.0: 1, 176264.0: 1, 176263.0: 3, 176290.0: 2, 176291.0: 3, 176790.0: 2, 176791.0: 1, 176818.0: 3, 179452.0: 3, 179453.0: 1, 179451.0: 2, 179424.0: 1, 179425.0: 2, 179952.0: 2, 179951.0: 1, 179978.0: 3, 179979.0: 1, 178399.0: 2, 178372.0: 1, 178370.0: 1, 178371.0: 2, 178400.0: 1, 178898.0: 2, 178899.0: 1, 178897.0: 1, 178926.0: 2, 178927.0: 1, 178925.0: 2, 173112.0: 3, 173111.0: 3, 173128.0: 2, 173129.0: 1, 173656.0: 1, 173655.0: 2, 173638.0: 3, 173637.0: 1, 172058.0: 1, 172059.0: 3, 172074.0: 3, 172075.0: 1, 172602.0: 1, 172601.0: 2, 172586.0: 2, 172585.0: 3, 175236.0: 2, 175237.0: 1, 175218.0: 1, 175216.0: 3, 175217.0: 2, 175214.0: 3, 175215.0: 3, 175212.0: 2, 175213.0: 3, 175210.0: 2, 175211.0: 1, 175209.0: 1, 175764.0: 3, 175763.0: 2, 175744.0: 3, 175745.0: 1, 175742.0: 1, 175743.0: 3, 175740.0: 1, 175738.0: 2, 175739.0: 2, 175736.0: 3, 175737.0: 2, 174164.0: 3, 174165.0: 2, 174182.0: 2, 174183.0: 1, 174710.0: 1, 174709.0: 2, 174690.0: 1, 174691.0: 3}
-
 
 // 수정 필요
 // grid - id popup
@@ -370,35 +290,35 @@ const gridStyle9 = (feature) => {
       fillcolor: 'white',
       fillOpacity: 0
       }
-  } else if (confirmed > 0 && confirmed < 10) {
+  } else if (confirmed === 1) {
       return {
       weight: 1, // stroke width (default: 3)
       color: '#1871D9',
       fillcolor: '#1871D9', 
-      fillOpacity: 0.2
+      fillOpacity: 0.1
       }
-  } else if (confirmed < 20) {
+  } else if (confirmed === 2) {
       return {
       weight: 1, // stroke width (default: 3)
       color: '#1871D9',
       fillcolor: '#1871D9', 
-      fillOpacity: 0.4
+      fillOpacity: 0.3
       }
-  } else if (confirmed < 30) {
+  } else if (confirmed === 3) {
       return {
       weight: 1, // stroke width (default: 3)
       color: '#1871D9',
       fillcolor: '#1871D9', 
-      fillOpacity: 0.6
+      fillOpacity: 0.5
       }
-  } else if (confirmed < 50) {
+  } else if (confirmed === 4) {
     return {
     weight: 1, // stroke width (default: 3)
     color: '#1871D9',
     fillcolor: '#1871D9', 
-    fillOpacity: 0.8
+    fillOpacity: 0.7
     }
-} else if (confirmed < 80) {
+} else if (confirmed === 5) {
   return {
   weight: 1, // stroke width (default: 3)
   color: '#1871D9',
@@ -406,12 +326,44 @@ const gridStyle9 = (feature) => {
   fillOpacity: 0.9
   }
 }
-else if (confirmed >= 80) {
+else if (confirmed === 6) {
   return {
   weight: 1, // stroke width (default: 3)
-  color: '#1871D9',
-  fillcolor: '#1871D9', 
-  fillOpacity: 1
+  color: '#1861d9',
+  fillcolor: '#1861d9', 
+  fillOpacity: 0.7
+  }
+}
+else if (confirmed === 7) {
+  return {
+  weight: 1, // stroke width (default: 3)
+  color: '#1861d9',
+  fillcolor: '#1861d9', 
+  fillOpacity: 0.7
+  }
+}
+else if (confirmed === 8) {
+  return {
+  weight: 1, // stroke width (default: 3)
+  color: '#1861d9',
+  fillcolor: '#1861d9', 
+  fillOpacity: 0.8
+  }
+}
+else if (confirmed === 9) {
+  return {
+  weight: 1, // stroke width (default: 3)
+  color: '#1861d9',
+  fillcolor: '#1861d9', 
+  fillOpacity: 0.8
+  }
+}
+else if (confirmed === 10) {
+  return {
+  weight: 1, // stroke width (default: 3)
+  color: '#1861d9',
+  fillcolor: '#1861d9', 
+  fillOpacity: 0.9
   }
 }
 }
@@ -537,7 +489,7 @@ else if (confirmed >= 80) {
               selected_polylines.map((polyline, i) => (
                 <Polyline 
                   key={i} 
-                  pathOptions={lineOptions[i]} 
+                  pathOptions={lineOptions[polyline[0][0].toString()[9]]} 
                   positions={polyline} 
                   onMouseOver={e => e.target.openPopup()}
                   onMouseOut={e => e.target.closePopup()}
